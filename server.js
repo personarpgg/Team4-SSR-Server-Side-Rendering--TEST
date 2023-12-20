a70505c953174d56bcaea52895350e1b066f8d2c
Garry <personarpg7501@gmail.com>
2023-12-20 17:53:27 +0900

    check : app.use를 사용하기위해 public 디렉토리로 파일 이동

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const app = express
+const app = express();
+const port = 8080;
+const path = require("path");
 
-
-
+app.use(express.static(path.join("__dirname", "public")));
 

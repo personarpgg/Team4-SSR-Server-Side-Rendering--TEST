627fcd8e5c7d56496b68226a6d2fa30d8225d637
Garry <personarpg7501@gmail.com>
2023-12-20 18:00:24 +0900

    check : server 테스트 오류 해결 완료(화살표 함수 띄어쓰기 문제)

--- server.js
+++ server.js
@@ -3,26 +3,14 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "public")));
-
-
-app.get("/", (res, req)=>{
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.listen(port, ()=>{
-    console.log(`서버오픈 : http://localhost${port}`)
-});
-
-
-/*
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.get('/', (req, res)=>{
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, ()=>{
   console.log(`Server listening at http://localhost:${port}`);
 });
-*/
+
+

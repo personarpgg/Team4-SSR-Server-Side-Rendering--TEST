bbaf1807c47bb206de2e3f512785ebf3639840b5
Garry <personarpg7501@gmail.com>
2023-12-20 17:54:03 +0900

    check : express get 방식으로 파일 서빙 테스트

--- server.js
+++ server.js
@@ -5,3 +5,16 @@ const path = require("path");
 
 app.use(express.static(path.join("__dirname", "public")));
 
+
+app.get("/", (res, req)=>{
+
+    res.sendFile(path.join("__dirname", "index.html"));
+
+});
+
+
+app.listen(port, ()=>{
+
+    console.log("서버 오픈" + `http://localhost:${port}`)
+});
+

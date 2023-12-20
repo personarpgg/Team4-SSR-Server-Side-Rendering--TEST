5837b10f77102f51742839678e4b1c850109bc65
Garry <personarpg7501@gmail.com>
2023-12-20 19:32:03 +0900

    check : SSR 테스트 완료

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>안녕 세계</h1>');
+    res.send('<h1>안녕 세계</h1>');
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+    console.log(`Server listening at http://localhost:${port}`);
 });

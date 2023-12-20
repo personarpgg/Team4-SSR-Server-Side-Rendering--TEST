b539ebab789d9c100ae42ecc47275b92413fb030
Garry <personarpg7501@gmail.com>
2023-12-20 19:31:21 +0900

    check : index.html 파일 없이 서버에서 html을 만들어 전달

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-const express = require("express");
+// server.js
+
+const express = require('express');
 const app = express();
-const port = 8080;
-const path = require("path");
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', (req, res) => {
+  res.send('<h1>안녕 세계</h1>');
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
-
-

8c55dfc196803e83acf83eef8e4fa55177ccd54d
Garry <personarpg7501@gmail.com>
2023-12-20 17:58:49 +0900

    check : server.js 테스트 중 오류 발생

--- server.js
+++ server.js
@@ -3,18 +3,26 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
-app.use(express.static(path.join("__dirname", "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 
 app.get("/", (res, req)=>{
-
-    res.sendFile(path.join("__dirname", "index.html"));
-
+    res.sendFile(path.join(__dirname, "index.html"));
 });
-
 
 app.listen(port, ()=>{
-
-    console.log("서버 오픈" + `http://localhost:${port}`)
+    console.log(`서버오픈 : http://localhost${port}`)
 });
 
+
+/*
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+*/
